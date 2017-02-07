35bf99120ebdff6c8148760d90ef8cea80d6eff5
Bogdan <andronchik.bogdan@gmail.com>
2017-02-07 20:27:11 +0200

    gamburger menu v.1

--- source-js/main.js
+++ source-js/main.js
@@ -1,17 +1,12 @@
-var menu = document.querySelector('.menu');
-function toggleMenu () {
-    menu.classList.toggle('open');
-}
-menu.addEventListener('click', toggleMenu);
-
-
 $(document).ready(function () {
     $('.menu').on('click', function () {
         $('.nav_container').slideToggle(500);
+        $('.menu').toggleClass('open');
     });
-    $(window).resize(function () {
+    $(window).on('resize',function () {
         if ($(window).width()>500){
             $('.nav_container').removeAttr('style');
+            $('.menu').removeClass('open');
         }
     })
 });
